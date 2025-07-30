933394e4273a8351d7c6915a5c67f33cf9c3c0b2
Loïc Leforestier <loicleforestier.pro@gmail.com>
2025-07-30 20:35:54 +0200

    added base functions

---
+++ game.py
@@ -0,0 +1,18 @@
+class Game():
+    def __init__(self):
+        pass
+    
+    def processInput():
+        pass
+    
+    def update(self):
+        pass
+    
+    def render(self):
+        pass
+    
+    def run(self):
+        self.processInput()
+        self.update()
+        self.render()        
+        self.clock.tick(60)
