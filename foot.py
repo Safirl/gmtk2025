f4796ecac2eb9d1556321aa152d5b41f5f3d559f
Héloïse P <heloise.pingitore@mmibordeaux.com>
2025-07-31 12:16:17 +0200

    fussion

--- foot.py
+++ foot.py
@@ -1,55 +1,50 @@
 import random
 class Foot():
-    def __init__(self):
-        self.legs : str = ""
-        self.foot : str = ""
+    def __init__(self, legsPath="", footPath="", hasLaces=True):
+        self.legsPath : str = legsPath
+        self.footPath : str = footPath
         self.x = random.randint(0, 700)
         self.y = random.randint(0, 500)
-        hasLaces: bool
-
-        self.legsOptions = [
-            "assets/legs1.png",
-            "assets/legs2.png",
-            "assets/legs3.png",
-            "assets/legs4.png"
-        ]
-    def __init__(self, legsPath="", footPath="", hasLaces=True):
-        self.legsPath = legsPath
-        self.footPath = footPath
         self.hasLaces = hasLaces
-    
+
+        self.foots = [
+    {
+        "legs": "assets/legs1.png",
+        "foot": "",
+        "hasLaces": True
+    },
+    {
+        "legs": "assets/legs2.png",
+        "foot": "",
+        "hasLaces": True
+    },
+    {
+        "legs": "assets/legs3.png",
+        "foot": "",
+        "hasLaces": True
+    },
+    {
+        "legs": "assets/legs4.png",
+        "foot": "",
+        "hasLaces": True
+    },
+]
+
+
+        
     def animate(seft):
         pass
 
     def clear(self):
         pass
     
-foots = [
-    {
-        "legs": "",
-        "foot": "",
-        "hasLaces": True
-    },
-    {
-        "legs": "",
-        "foot": "",
-        "hasLaces": True
-    },
-    {
-        "legs": "",
-        "foot": "",
-        "hasLaces": True
-    },
-    {
-        "legs": "",
-        "foot": "",
-        "hasLaces": True
-    },
-]
+
 
     def getRandomLegs(self):
-        self.legs = random.choice(self.legsOptions)
+        # self.legs = random.choice(self.foots["legs"])
+        pass
     
     def draw(self, surface):
-        if self.legs:
-            surface.blit(self.legs, (self.x, self.y))
+        # if self.legs:
+        #     surface.blit(self.legs, (self.x, self.y))
+        pass
